1e06add7e562878d606ba9095cb991cec4c24094
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-01-25 20:04:54 +0100

    dadi js final commit

--- dadi/main.js
+++ dadi/main.js
@@ -1,15 +1,34 @@
-let playerNumbers = [Math.floor(Math.random() * 7 + 1), Math.floor(Math.random() * 7 + 1)];
-let computerNumbers = [Math.floor(Math.random() * 7 + 1), Math.floor(Math.random() * 7 + 1)];
+let playerNumber = Math.floor(Math.random() * 6 + 1);
+let computerNumber = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById('player-number').innerHTML = 'Il tuo numero è: ' + playerNumber;
+document.getElementById('computer-number').innerHTML = 'Il numero del computer è: ' + computerNumber;
+
+if (playerNumber > computerNumber) {
+    document.getElementById('winner').innerHTML = 'Vinci tu!';
+} else if (playerNumber < computerNumber) {
+    document.getElementById('winner').innerHTML = 'Vince il computer!';
+} else {
+    document.getElementById('winner').innerHTML = 'Avete pareggiato!';
+}
+
+
+/* Gioco con due dadi
+
+let playerNumbers = [Math.floor(Math.random() * 6 + 1), Math.floor(Math.random() * 6 + 1)];
+let computerNumbers = [Math.floor(Math.random() * 6 + 1), Math.floor(Math.random() * 6 + 1)];
 let playerSum = playerNumbers[0] + playerNumbers[1];
 let computerSum = computerNumbers[0] + computerNumbers[1];
 
-console.log('I tuoi numeri sono: ' + playerNumbers[0] + ', ' + playerNumbers[1]);
-console.log('I numeri del computer sono: ' + computerNumbers[0] + ', ' + computerNumbers[1]);
+document.getElementById('player-number').innerHTML = 'I tuoi numeri sono: ' + playerNumbers[0] + ', ' + playerNumbers[1];
+document.getElementById('computer-number').innerHTML = 'I numeri del computer sono: ' + computerNumbers[0] + ', ' + computerNumbers[1];
 
 if (playerSum > computerSum) {
-    console.log('Vinci tu!');
+    document.getElementById('winner').innerHTML = 'Vinci tu!';
 } else if (playerSum < computerSum) {
-    console.log('Vince il computer!');
+    document.getElementById('winner').innerHTML = 'Vince il computer!';
 } else {
-    console.log('Avete pareggiato!');
+    document.getElementById('winner').innerHTML = 'Avete pareggiato!';
 }
+
+*/
